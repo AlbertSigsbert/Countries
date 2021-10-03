33653cad2f78a52a8ac31f20f9c4be39d5352284
Albert <ishengomaalbert@gmail.com>
2021-10-03 06:34:43 +0300

    Nav UI & DarkMode Toggle

---
+++ src/app.js
@@ -0,0 +1,47 @@
+//DOM Vars
+const iconToggle = document.getElementById('toggle-icon');
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+//Theme Vars
+const bgColor = '--bg-color';
+const txtColor = '--text-color';
+const elmntColor = '--elements-color';
+let darkMode = true
+
+
+const switchToLightMode  = () => {
+    document.documentElement.style.setProperty(bgColor, 'hsl(0, 0%, 98%)');
+    document.documentElement.style.setProperty(txtColor, 'hsl(200, 15%, 8%)');
+    document.documentElement.style.setProperty(elmntColor, 'hsl(0, 0%, 100%)');
+    iconToggle.classList.remove('fas');
+    iconToggle.classList.add('far');
+    localStorage.setItem('isDark', 'no');
+    darkMode = false;
+ 
+};
+
+const switchToDarkMode  = () => {
+    document.documentElement.style.setProperty(bgColor, 'hsl(207, 26%, 17%)');
+    document.documentElement.style.setProperty(txtColor, 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty(elmntColor, 'hsl(209, 23%, 22%)');
+    iconToggle.classList.remove('far');
+    iconToggle.classList.add('fas');
+    localStorage.setItem('isDark', 'yes');
+    darkMode = true;
+ 
+};
+
+//On Document Load
+document.addEventListener('DOMContentLoaded', () => {
+    const isDark = localStorage.getItem('isDark');
+     (isDark === 'yes') ?    switchToDarkMode() : switchToLightMode();
+       
+})
+
+//On icon Click
+darkModeToggle.addEventListener('click', e => {
+    darkMode ? switchToLightMode() :  switchToDarkMode();
+
+    e.preventDefault();
+})
+
