bc64842ac30d3ecd685cb2254151b3394ac33592
Albert <ishengomaalbert@gmail.com>
2021-10-04 07:14:28 +0300

    Styled search & dropdown components

--- src/app.js
+++ src/app.js
@@ -6,13 +6,15 @@ const darkModeToggle = document.getElementById('dark-mode-toggle');
 const bgColor = '--bg-color';
 const txtColor = '--text-color';
 const elmntColor = '--elements-color';
-let darkMode = true
+const inptColor = '--input-color';
+let darkMode = true;
 
 
 const switchToLightMode  = () => {
     document.documentElement.style.setProperty(bgColor, 'hsl(0, 0%, 98%)');
     document.documentElement.style.setProperty(txtColor, 'hsl(200, 15%, 8%)');
     document.documentElement.style.setProperty(elmntColor, 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty(inptColor, 'hsl(0, 0%, 52%)');
     iconToggle.classList.remove('fas');
     iconToggle.classList.add('far');
     localStorage.setItem('isDark', 'no');
@@ -24,6 +26,7 @@ const switchToDarkMode  = () => {
     document.documentElement.style.setProperty(bgColor, 'hsl(207, 26%, 17%)');
     document.documentElement.style.setProperty(txtColor, 'hsl(0, 0%, 100%)');
     document.documentElement.style.setProperty(elmntColor, 'hsl(209, 23%, 22%)');
+    document.documentElement.style.setProperty(inptColor, 'hsl(0, 0%, 100%)');
     iconToggle.classList.remove('far');
     iconToggle.classList.add('fas');
     localStorage.setItem('isDark', 'yes');
@@ -45,3 +48,11 @@ darkModeToggle.addEventListener('click', e => {
     e.preventDefault();
 })
 
+//Dropdown toggle
+const dropBtn = document.querySelector('.dropbtn');
+const dropContent = document.querySelector('.dropdown-content');
+
+dropBtn.addEventListener('click', e => {
+    dropContent.classList.toggle('show');
+    e.preventDefault();
+})
