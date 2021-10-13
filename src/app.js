ef14d91be5fee3536591c918f84da5087329cf55
Albert <ishengomaalbert@gmail.com>
2021-10-13 14:09:04 +0300

    Fetch data from API,Render data to UI & Finish styling homepage

--- src/app.js
+++ src/app.js
@@ -7,14 +7,22 @@ const bgColor = '--bg-color';
 const txtColor = '--text-color';
 const elmntColor = '--elements-color';
 const inptColor = '--input-color';
+const boxShadow = '--box-shadow';
 let darkMode = true;
 
+
+const refresh =  () =>  {    
+    setTimeout(() => {
+        window.location.reload();
+    }, 50);
+}
 
 const switchToLightMode  = () => {
     document.documentElement.style.setProperty(bgColor, 'hsl(0, 0%, 98%)');
     document.documentElement.style.setProperty(txtColor, 'hsl(200, 15%, 8%)');
     document.documentElement.style.setProperty(elmntColor, 'hsl(0, 0%, 100%)');
     document.documentElement.style.setProperty(inptColor, 'hsl(0, 0%, 52%)');
+    document.documentElement.style.setProperty(boxShadow,'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;');
     iconToggle.classList.remove('fas');
     iconToggle.classList.add('far');
     localStorage.setItem('isDark', 'no');
@@ -27,6 +35,8 @@ const switchToDarkMode  = () => {
     document.documentElement.style.setProperty(txtColor, 'hsl(0, 0%, 100%)');
     document.documentElement.style.setProperty(elmntColor, 'hsl(209, 23%, 22%)');
     document.documentElement.style.setProperty(inptColor, 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty(boxShadow,'none');
+
     iconToggle.classList.remove('far');
     iconToggle.classList.add('fas');
     localStorage.setItem('isDark', 'yes');
@@ -44,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
 //On icon Click
 darkModeToggle.addEventListener('click', e => {
     darkMode ? switchToLightMode() :  switchToDarkMode();
-
+    refresh();
     e.preventDefault();
 })
 
@@ -56,3 +66,47 @@ dropBtn.addEventListener('click', e => {
     dropContent.classList.toggle('show');
     e.preventDefault();
 })
+
+//API REQUESTS
+axios.get('https://restcountries.com/v3.1/all')
+.then(function (response) {
+    // handle success
+    // console.log(response.data);
+      
+    const countriesData = response.data;
+
+     //Render to UI
+     const countries = document.querySelector('.countries');
+
+     let output = '';
+     countriesData.forEach(country => {
+         
+
+          output += `
+            <div class="card">
+            <img class="card-img" src="${country.flags.svg}" alt="">
+            <div class="card-body">
+                <h3 class="card-title">${country.name.common}</h3>
+                <ul>
+                  <li>Population: ${country.population}</li>
+                  <li>Region: ${country.region}</li>
+                  <li>Capital: ${country.capital}</li>
+                </ul>
+               
+            </div>
+            </div>
+          `;
+
+          
+    });
+    
+    countries.innerHTML = output;
+
+ 
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
+
